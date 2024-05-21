0554062b6a2329b32b1db9f02cb9e76f20c0ed17
Antoniotaleal <139480765+Antoniotaleal@users.noreply.github.com>
2024-05-21 12:30:47 +0100

    Desafio numero 6 completo

--- js/desafio6.js
+++ js/desafio6.js
@@ -1,22 +1,24 @@
 function calculaTarifa(idade, tipoUsuario){
 
-    switch(tipoUsuario, idade) { 
-        case 'estudante':
-            return 1.25;
-            break;
-        case 'regular':
-            return 2.50;
-        case 'menor de 6 anos':
-                return 0;
-                break;
-        case 'maior de 60 anos':
-                return 1.75;
-    }
+    if(idade > 60){
+        return 2.50 * 30 / 100;
+    };
+
+    if(idade < 6){
+        return 0;
+    };
+    
+    if(tipoUsuario == 'estudante'){
+        return 2.50 * 50 / 100;
+    };
+
+    if(tipoUsuario == 'regular'){
+        return 2.50;
+    };
+
 }
-    let tarifaIdade = calculaTarifa('maior de 60 anos');
-    console.log (tarifaIdade);
-    let tarifaUsuario = calculaTarifa('');
-    console.log (tarifaUsuario);
+    let tarifa = calculaTarifa(70, 'regular');
+    console.log (tarifa);
     
 
     
---
+++ js/desafio7.js
@@ -0,0 +1,11 @@
+let num = 9;
+let i = 1;
+let text = "";
+function somaNumeros(num){
+    
+    for (let i = 1; i < num; i++) { 
+    }
+}
+somaNumeros(i < 9)
+    console.log( )
+  
