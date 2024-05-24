85bf078bf0912c9853862e4f3e30ab9401417e1e
Antoniotaleal <139480765+Antoniotaleal@users.noreply.github.com>
2024-05-24 11:02:10 +0100

    Primeiro commit do desafio 8

--- js/desafio7.js
+++ js/desafio7.js
@@ -2,7 +2,7 @@ function somaNumeros(num)
 {
     let resultado = 0;
     for (let i = 1; i <= num; i++) { 
-        resultado = (resultado + i)
+        resultado = (num * i)
     }
     console.log(resultado)
 }
---
+++ js/desafio8.js
@@ -0,0 +1,8 @@
+function gerarTabuada(num){
+    let n = 0;
+    for (let i = 1; i <= num; i++) { 
+        n = (i*num)
+    }
+    console.log(n)
+}
+
