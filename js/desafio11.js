649349f7d4feeeacc27fb308f4812b8d65079dc0
Antoniotaleal <139480765+Antoniotaleal@users.noreply.github.com>
2024-06-06 15:07:46 +0100

    desafio 11 completo

---
+++ js/desafio11.js
@@ -0,0 +1,6 @@
+let nome = " Meu nome é Antonio Teixeira de Almeida Leal";
+let idade = " tenho 14 anos";
+let cidade = " e moro em Santo Tirso";
+let inf = (nome + " " + idade + " " + cidade);
+console.log(inf);
+
