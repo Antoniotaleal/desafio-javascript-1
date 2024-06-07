7f42759c56bdf47307412105766bc821cc80e08d
Antoniotaleal <139480765+Antoniotaleal@users.noreply.github.com>
2024-06-07 11:55:50 +0100

    desafio 12

---
+++ js/desafio12.js
@@ -0,0 +1,7 @@
+function inverseString (str) {
+    str = "Hello world!";
+    let result = str.slice(6);
+    let resulta = str.slice(0 , 5)
+    console.log(result + " " + resulta)
+}
+inverseString()
