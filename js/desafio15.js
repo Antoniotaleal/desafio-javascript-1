d1e560b65a40c30a8b3a7bbff3707ae84a4ffed5
Antoniotaleal <139480765+Antoniotaleal@users.noreply.github.com>
2024-06-20 12:40:03 +0100

    desafio 15 finalizado

---
+++ js/desafio15.js
@@ -0,0 +1,15 @@
+function number(n, m){
+    let resultado = "";
+    if(m > n){
+        console.log(resultado = n + " " + "é o número menor")
+    }
+    if(m < n){
+        console.log(resultado = m + " " + "é o número menor")
+    }
+    if(m > n){
+        console.log(resultado = m + " " + "é o número maior")
+    }
+    if(m < n){
+        console.log(resultado = n + " " + "é o número maior")
+    }
+}
